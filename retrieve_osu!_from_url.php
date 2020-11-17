79c5b84a121b69d5395d72b78c03a7a3c141a825
Thestickman391 <stickman391@gmail.com>
2020-11-17 19:49:16 +1300

    Fixed security issue with potential ACE

--- retrieve_osu!_from_url.php
+++ retrieve_osu!_from_url.php
@@ -2,28 +2,33 @@
 	
 	//$download_url = "https://bloodcat.com/osu/s/244796"; 
 	$download_url = $_GET["download_url"];
-	$headers = get_headers($download_url,true);
-	$headers = array_combine(array_map("strtolower",array_keys($headers)),$headers);
-	$fileName = isset($headers['content-disposition']) ? strstr($headers['content-disposition'], "=") : null ;
-	$fileName = trim($fileName,"=\"'");
-	$fileName = (explode('";', $fileName)[0]);
-	$file = "./tmp/" . urldecode($fileName);
-	$script = basename($_SERVER['PHP_SELF']);
+	if(substr($download_url,0,27) != "https://bloodcat.com/osu/s/")
+	{
+		echo "Not an osu! url!";
+	} else {
+		$headers = get_headers($download_url,true);
+		$headers = array_combine(array_map("strtolower",array_keys($headers)),$headers);
+		$fileName = isset($headers['content-disposition']) ? strstr($headers['content-disposition'], "=") : null ;
+		$fileName = trim($fileName,"=\"'");
+		$fileName = (explode('";', $fileName)[0]);
+		$file = "./tmp/" . urldecode($fileName);
+		$script = basename($_SERVER['PHP_SELF']);
 
-// download the file 
-	file_put_contents($file, fopen($download_url, 'r'));
-	echo '&fileName=' . urldecode($fileName);
-	$zip = new ZipArchive;
-	$res = $zip->open($file);
-	if ($res === TRUE) {
-		$charts = array();
-		echo '&charts=';
-		for( $i = 0; $i < $zip->numFiles; $i++ ){ 
-			$stat = $zip->statIndex( $i ); 
-			if (substr($stat['name'], -4) == '.osu'){
-					echo ( basename( $stat['name'] )); 
-					array_push($charts, basename( $stat['name'] ));
+	// download the file 
+		file_put_contents($file, fopen($download_url, 'r'));
+		echo '&fileName=' . urldecode($fileName);
+		$zip = new ZipArchive;
+		$res = $zip->open($file);
+		if ($res === TRUE) {
+			$charts = array();
+			echo '&charts=';
+			for( $i = 0; $i < $zip->numFiles; $i++ ){ 
+				$stat = $zip->statIndex( $i ); 
+				if (substr($stat['name'], -4) == '.osu'){
+						echo ( basename( $stat['name'] )); 
+						array_push($charts, basename( $stat['name'] ));
+					}
 				}
-			}
+		}
 	}
 ?>
